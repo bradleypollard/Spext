75eeb0a609e96d98a1b8b7af769d39846a021cc6
Bradley Pollard <bradley.pollard12@ic.ac.uk>
2013-11-18 21:47:00 +0000

    Initial commit

---
+++ Assets/Code/GameLogic.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	private int day = 0;
+	private int minerals = 0;
+	private int research = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void incrementDay() {
+		day++;
+		Debug.Log("Day is: " + day);
+	}
+}
---
+++ Assets/Code/Overlay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Overlay : MonoBehaviour {
+
+	public GameObject gameLogic;
+	private GameLogic logicScript;
+	private int screenIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (logicScript == null) {
+			logicScript = (GameLogic)gameLogic.GetComponent("GameLogic");
+		}
+	}
+
+	public void setScreen(string s) {
+		if (s == "Military") {
+			screenIndex = 0;
+		} else if (s == "Planets") {
+			screenIndex = 1;
+		} else if (s == "Research") {
+			screenIndex = 2;
+		} else if (s == "Resources") {
+			screenIndex = 3;
+		}
+
+		Debug.Log("Name is: " + s + " and index is: " + screenIndex);
+	}
+}
