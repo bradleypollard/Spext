2606e6047ce46f3e8db6de90db32934b8a2e0b63
Bradley Pollard <bradley.pollard12@ic.ac.uk>
2013-11-19 01:02:22 +0000

    Missing files added

---
+++ Assets/Code/Buttons/BoxButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxButton : ButtonMouseOver {
+
+	// Use this for initialization
+	void Start () {
+		r = (SpriteRenderer)gameObject.GetComponent("SpriteRenderer");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnMouseDown() {
+		// TODO
+	}
+}
